b24bcadbdb7e39c50f567c8cb544efe503bb49cb
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-08-02 23:11:53 +0300

    fixed

--- python/code_challenges/linked-list/linked_list/linked_list.py
+++ python/code_challenges/linked-list/linked_list/linked_list.py
@@ -6,46 +6,43 @@ class Node():
     def __str__(self):
         return str(self.value)
 
-
 class LinkedList():
     def __init__(self):
         self.head = None
 
-    def insert(self, value):
-        node = Node(value)
+    def insert(self, data):
+        node = Node(data)
         if self.head:
             node.next = self.head
         self.head = node
 
-    def includes(self, value):
+    def includes(self, data):
         boolean = False
         current = self.head
-
         while current:
-            if current.value == value:
+            if current.value == data:
                 boolean = True
-            else:
-                current = current.next
+                break
+            current = current.next
         return boolean
 
     def __str__(self):
-        string = ""
-        current = self.head
-
-        while current:
-            string += f"{str(current.value)} -> "
-            current = current.next
-        string += "None"
-        return string
+            string = ""
+            current = self.head
+            while current:
+                string += f"{str(current.value)} -> "
+                current = current.next
+            string += "None"
+            return string
 
     def __iter__(self):
-        new_list = []
+        # new_list = []
         current = self.head
 
         while current:
             yield current.value
             current = current.next
-        return new_list
+        # return new_list
 
     def __repr__(self):
         return "LinkedList()"
@@ -57,11 +54,8 @@ if __name__ == "__main__":
     ll.insert(9)
     ll.insert(11)
     print(ll)
-    # lol = str(ll)
-    # x = " {{ lol }} {0} "
-    # print(x.format('->'))
     node1 = Node(1)
     node2 = Node(2)
-
+    print(node1)
     for value in ll:
         print(value)
